6c3194b4e905befec16d5d03c990390fbda1d681
dncoble <dncoble@gmail.com>
2021-09-13 13:39:27 -0400

    save weights as csv

--- load_preprocess.py
+++ load_preprocess.py
@@ -76,7 +76,7 @@ def preprocess():
     sav_X_test.close()
     sav_y_test.close()
     sav_t_test.close()
-
+# i don't think this actually works properly, but i'll fix it if i ever need it
 def save_model_weights_as_json(model, savpath="model_weights.json"):
     import json
     i = 0
@@ -88,6 +88,44 @@ def save_model_weights_as_json(model, savpath="model_weights.json"):
         i += 1
     with open(savpath, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+# save model weights as folder of csvs.
+def save_model_weights_as_csv(model, savpath = "./model_weights"):
+    import os
+    import os.path as path
+    from numpy import savetxt
+    if(not path.exists(savpath)):
+        os.mkdir(savpath)
+    for layer in model.layers[:-1]: # the final layer is a dense top
+        layer_path = savpath + "./" + layer.name + "./"
+        if(not path.exists(layer_path)):
+            os.mkdir(layer_path)
+        W, U, b = layer.get_weights()
+        units = U.shape[0]
+        
+        savetxt(layer_path+"Wi.csv",W[:,:units],delimiter=',')
+        savetxt(layer_path+"Wf.csv",W[:,units:units*2],delimiter=',')
+        savetxt(layer_path+"Wc.csv",W[:,units*2:units*3],delimiter=',')
+        savetxt(layer_path+"Wo.csv",W[:,units*3:],delimiter=',')
+        savetxt(layer_path+"Ui.csv",U[:,:units],delimiter=',')
+        savetxt(layer_path+"Uf.csv",U[:,units:units*2],delimiter=',')
+        savetxt(layer_path+"Uc.csv",U[:,units*2:units*3],delimiter=',')
+        savetxt(layer_path+"Uo.csv",U[:,units*3:],delimiter=',')
+        savetxt(layer_path+"bi.csv",b[:units],delimiter=',')
+        savetxt(layer_path+"bf.csv",b[units:units*2],delimiter=',')
+        savetxt(layer_path+"bc.csv",b[units*2:units*3],delimiter=',')
+        savetxt(layer_path+"bo.csv",b[units*3:],delimiter=',')
+    
+    #save dense top layer
+    dense_top = model.layers[-1]
+    in_weights, out_weights = dense_top.get_weights()
+    layer_path = savpath + "./dense_top./"
+    if(not path.exists(layer_path)):
+        os.mkdir(layer_path)    
+    savetxt(layer_path+"in_weights.csv",in_weights,delimiter=',')
+    savetxt(layer_path+"out_weights.csv",out_weights,delimiter=',')
+
+
 
 if __name__ == '__main__':
     preprocess()
